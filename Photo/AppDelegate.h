e75c8d96cc41ad5442707729e3450e886d40208b
Crystalcxd <547339930@qq.com>
2015-10-24 10:39:39 +0800

    Initial Commit

---
+++ Photo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo
+//
+//  Created by Michael on 15/10/24.
+//  Copyright © 2015年 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
